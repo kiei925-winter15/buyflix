91793938246f089c5f4fbdb014c10441ad5320e4
Brian Eng <brian@brianeng.com>
2015-02-11 16:48:03 -0600

    more week 6 prep

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,4 +13,27 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def create
+    movie_params = params.require(:movie).permit!
+    Movie.create(movie_params)
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find_by(id: params["id"])
+  end
+
+  def update
+    movie_params = params.require(:movie).permit!
+    @movie = Movie.find_by(id: params["id"])
+    @movie.update(movie_params)
+    redirect_to movies_path
+  end
+
+  def destroy
+    @movie = Movie.find_by(id: params["id"])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
 end
